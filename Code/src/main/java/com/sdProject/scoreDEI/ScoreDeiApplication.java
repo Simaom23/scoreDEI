1019195fa611c40f8e6bee838daa22a5b57b9812
Simaom23 <simaocm23@gmail.com>
2022-05-09 12:39:33 +0100

    Spring Initilzr

---
+++ Code/src/main/java/com/sdProject/scoreDEI/ScoreDeiApplication.java
@@ -0,0 +1,13 @@
+package com.sdProject.scoreDEI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoreDeiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoreDeiApplication.class, args);
+	}
+
+}
---
+++ Code/src/test/java/com/sdProject/scoreDEI/ScoreDeiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdProject.scoreDEI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoreDeiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
