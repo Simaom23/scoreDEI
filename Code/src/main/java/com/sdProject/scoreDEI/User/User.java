45b2b716e323c90b8a55da5e9879f250bb6b5ffa
Simaom23 <simaocm23@gmail.com>
2022-05-16 21:51:56 +0100

    Added addUser

--- Code/src/main/java/com/sdProject/scoreDEI/User/User.java
+++ Code/src/main/java/com/sdProject/scoreDEI/User/User.java
@@ -9,16 +9,65 @@ import javax.persistence.Id;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
-    private String name, email, telephone, role;
+    private long id;
+    private String name, email, telephone, role, password;
 
     public User() {
     }
 
-    public User(String name, String email, String telephone, String role) {
+    public User(String name, String email, String password, String telephone, String role) {
         this.name = name;
         this.email = email;
+        this.password = password;
         this.telephone = telephone;
+        this.role = role;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void getPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
         this.role = role;
     }
 }
--- Code/src/main/java/com/sdProject/scoreDEI/User/UserController.java
+++ Code/src/main/java/com/sdProject/scoreDEI/User/UserController.java
@@ -2,6 +2,9 @@ package com.sdProject.scoreDEI.User;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
 
 @Controller
 public class UserController {
@@ -12,7 +15,7 @@ public class UserController {
     }
 
     @GetMapping("/login")
-    public String getLogin() {
+    public String login() {
         return "login";
     }
 
@@ -21,4 +24,15 @@ public class UserController {
         return "homepage";
     }
 
+    @GetMapping("/addUser")
+    public String addUser(Model model) {
+        model.addAttribute("user", new User());
+        return "addUser";
+    }
+
+    @PostMapping("/saveUser")
+    public String saveUser(@ModelAttribute User user, Model model) {
+        model.addAttribute("user", user);
+        return "redirect:/layout";
+    }
 }
