a445dce475435da8b3c421ad2a4e9c49f9716ada
MarcoPais0 <marco.cruz.pais@gmail.com>
2022-05-30 17:38:00 +0100

    loadTeams feature done

--- Code/src/main/java/com/sdProject/scoreDEI/Team/TeamController.java
+++ Code/src/main/java/com/sdProject/scoreDEI/Team/TeamController.java
@@ -2,6 +2,8 @@ package com.sdProject.scoreDEI.Team;
 
 import java.util.Optional;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,20 +12,56 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
 @Controller
 public class TeamController {
     @Autowired
     TeamService teamService;
 
+    @GetMapping("/loadTeams")
+    public String loadTeams(Model model) throws Exception{
+        
+        Unirest.setTimeouts(0, 0);
+        String host = "https://v3.football.api-sports.io/";
+        String query = "/teams?league=2&season=2021";
+
+        HttpResponse<JsonNode> response = Unirest.get(host + query)
+        .header("x-rapidapi-key", "b43ff12ff9935aac041a44e7554f52be")
+        .header("x-rapidapi-host", "v3.football.api-sports.io")
+        .asJson();
+        
+        // Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        // JsonParser jp = new JsonParser();
+        // JsonElement je = jp.parse(response.getBody().toString());
+        // String prettyJsonString = gson.toJson(je);
+        // System.out.println("status\n" + response.getStatus() + "\n");
+        // System.out.println("pretty\n" + prettyJsonString + "\n");
+
+        JSONArray jo = (JSONArray) response.getBody().getObject().get("response");
+        for (int i = 0; i < jo.length(); i++) {
+            //System.out.println(jo.get(i));
+        
+            JSONObject object = (JSONObject) jo.get(i);
+            //JSONObject venue = (JSONObject) object.get("venue");
+            JSONObject team = (JSONObject) object.get("team");
+
+            Team t = new Team(team.getString("name"));
+            this.teamService.addTeam(t);
+        }
+
+        return "redirect:/listTeams";
+    }
+
     @GetMapping("/createTeam")
     public String createTeam(Model model) {
-        Team[] teams = {
-                new Team("Sporting CP"),
-                new Team("SL Benfica"),
-                new Team("FC Porto")
-        };
-        for (Team team : teams)
-            this.teamService.addTeam(team);
         model.addAttribute("team", new Team());
         return "createTeam";
     }
