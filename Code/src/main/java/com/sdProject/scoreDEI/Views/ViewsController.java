9f001bea97227a3ebc11b5238af2892fd9cfe0fa
Simaom23 <simaocm23@gmail.com>
2022-06-01 11:37:55 +0100

    Finish v0.1

--- Code/src/main/java/com/sdProject/scoreDEI/Views/ViewsController.java
+++ Code/src/main/java/com/sdProject/scoreDEI/Views/ViewsController.java
@@ -1,7 +1,9 @@
 package com.sdProject.scoreDEI.Views;
 
+import java.text.SimpleDateFormat;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -41,19 +43,32 @@ public class ViewsController {
     @GetMapping("/gameStats")
     public String gameStats(@RequestParam(name = "id", required = true) int id, Model model) {
         Optional<Game> op = this.gameService.getGameById(id);
+        long time = -1;
         if (op.isPresent()) {
             Game game = op.get();
             List<Event> events = game.getEvents();
             Collections.sort(events, new Comparator<Event>() {
                 @Override
                 public int compare(Event e1, Event e2) {
-                    return e1.getDate().compareTo(e2.getDate());
+                    return e1.getTime().compareTo(e2.getTime());
                 }
             });
+            if (game.getStarted()) {
+                SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+                try {
+                    Date time1 = format.parse(java.time.LocalTime.now().toString());
+                    Date time2 = format.parse(game.getTime().toString());
+                    time = time1.getTime() - time2.getTime();
+                    time = (time / (60 * 1000)) + 60;
+                } catch (Exception e) {
+                    time = -1;
+                }
+            }
             model.addAttribute("game", game);
             model.addAttribute("homeGoals", this.gameService.getTeamGoals(game.getHomeTeam()));
             model.addAttribute("awayGoals", this.gameService.getTeamGoals(game.getAwayTeam()));
             model.addAttribute("events", events);
+            model.addAttribute("gameTime", time);
             return "gameStats";
         } else {
             return "redirect:/homepage";
@@ -63,11 +78,32 @@ public class ViewsController {
     @GetMapping("/gameStatsUser")
     public String gameStatsUser(@RequestParam(name = "id", required = true) int id, Model model) {
         Optional<Game> op = this.gameService.getGameById(id);
+        long time = -1;
         if (op.isPresent()) {
             Game game = op.get();
+            List<Event> events = game.getEvents();
+            Collections.sort(events, new Comparator<Event>() {
+                @Override
+                public int compare(Event e1, Event e2) {
+                    return e1.getTime().compareTo(e2.getTime());
+                }
+            });
+            if (game.getStarted()) {
+                SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+                try {
+                    Date time1 = format.parse(java.time.LocalTime.now().toString());
+                    Date time2 = format.parse(game.getTime().toString());
+                    time = time1.getTime() - time2.getTime();
+                    time = (time / (60 * 1000)) + 60;
+                } catch (Exception e) {
+                    time = -1;
+                }
+            }
             model.addAttribute("game", game);
             model.addAttribute("homeGoals", this.gameService.getTeamGoals(game.getHomeTeam()));
             model.addAttribute("awayGoals", this.gameService.getTeamGoals(game.getAwayTeam()));
+            model.addAttribute("events", events);
+            model.addAttribute("gameTime", time);
             return "gameStatsUser";
         } else {
             return "redirect:/homepageUser";
@@ -77,11 +113,32 @@ public class ViewsController {
     @GetMapping("/gameStatsUnsigned")
     public String gameStatsUnsigned(@RequestParam(name = "id", required = true) int id, Model model) {
         Optional<Game> op = this.gameService.getGameById(id);
+        long time = -1;
         if (op.isPresent()) {
             Game game = op.get();
+            List<Event> events = game.getEvents();
+            Collections.sort(events, new Comparator<Event>() {
+                @Override
+                public int compare(Event e1, Event e2) {
+                    return e1.getTime().compareTo(e2.getTime());
+                }
+            });
+            if (game.getStarted()) {
+                SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+                try {
+                    Date time1 = format.parse(java.time.LocalTime.now().toString());
+                    Date time2 = format.parse(game.getTime().toString());
+                    time = time1.getTime() - time2.getTime();
+                    time = (time / (60 * 1000)) + 60;
+                } catch (Exception e) {
+                    time = -1;
+                }
+            }
             model.addAttribute("game", game);
             model.addAttribute("homeGoals", this.gameService.getTeamGoals(game.getHomeTeam()));
             model.addAttribute("awayGoals", this.gameService.getTeamGoals(game.getAwayTeam()));
+            model.addAttribute("events", events);
+            model.addAttribute("gameTime", time);
             return "gameStatsUnsigned";
         } else {
             return "redirect:/homepageUnsigned";
