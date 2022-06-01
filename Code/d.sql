f3f8c557f1d2db26ae5e367053cc3492c179969f
Simaom23 <simaocm23@gmail.com>
2022-06-01 11:55:08 +0100

    Finish v1

--- Code/d.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO Person(id, email, name, password, role, telephone) VALUES(1000, 'admin@admin', 'admin', 'admin', 'true', DEFAULT);
-INSERT INTO Person(id, email, name, password, role, telephone) VALUES(1001, 'user@user', 'user', 'user', 'false', DEFAULT);
---
+++ Code/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO Person(id, email, name, password, role, telephone) VALUES(1000, 'admin@admin', 'admin', 'admin', 'true', 'DEFAULT');
+INSERT INTO Person(id, email, name, password, role, telephone) VALUES(1001, 'user@user', 'user', 'user', 'false', 'DEFAULT');
