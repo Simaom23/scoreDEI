d5ccfb469dde7e9b798b12178ed7d8de72785bd9
MarcoPais0 <marco.cruz.pais@gmail.com>
2022-06-01 20:05:48 +0100

    war file

--- Code/src/main/java/com/sdProject/scoreDEI/ScoreDeiApplication.java
+++ Code/src/main/java/com/sdProject/scoreDEI/ScoreDeiApplication.java
@@ -3,15 +3,21 @@ package com.sdProject.scoreDEI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories("com.sdProject.*")
 @EntityScan("com.sdProject.*")
-public class ScoreDeiApplication {
+public class ScoreDeiApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScoreDeiApplication.class, args);
 	}
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ScoreDeiApplication.class);
+    }
 }
